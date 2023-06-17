3a46c7684733941992a9e2ad59bc4b6e306d972a
Eyshcharya <eyshcharya@email.com>
2023-06-17 10:21:09 +0530

    Contact Page

---
+++ src/Pages/Contacts.jsx
@@ -0,0 +1,49 @@
+import SharedLayout from '../Components/SharedLayout';
+import SingleContact from '../Components/SingleContact';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import SaveModal from '../Components/SaveModal';
+import DeleteModal from '../Components/DeleteModal';
+
+const Contacts = () => {
+  const { contactArray } = useSelector((store) => store.contact);
+
+  const { isSaveModelOpen, isDeleteSuccessOpen } = useSelector(
+    (store) => store.modal
+  );
+
+  return (
+    <>
+      <SharedLayout />
+      {isSaveModelOpen && <SaveModal />}
+      {isDeleteSuccessOpen && <DeleteModal />}
+
+      <div className='contacts-page'>
+        <button id='add-contact-btn3'>
+          <Link to='/contacts/new'>Add new Contact</Link>
+        </button>
+        <div className='contact-head'>
+          <p>
+            <span>Contacts</span>
+          </p>
+        </div>
+
+        <div className='contacts-list'>
+          <div className='list-head'>
+            <div> </div>
+            <div>Full Name</div>
+            <div>Gender</div>
+            <div>E-mail</div>
+            <div>Phone Number</div>
+          </div>
+          {contactArray.map((contact, index) => {
+            return (
+              <SingleContact key={contact.id} {...contact} index={index} />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+export default Contacts;
