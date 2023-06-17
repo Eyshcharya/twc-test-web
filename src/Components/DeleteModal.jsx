458bfe838d753f5d4a5d57adcc711088e3984c5d
Eyshcharya <eyshcharya@email.com>
2023-06-17 10:17:15 +0530

    Modal

---
+++ src/Components/DeleteModal.jsx
@@ -0,0 +1,27 @@
+import { useDispatch } from 'react-redux';
+import { CloseDeleteSuccessModal } from '../Features/ModalSlice';
+
+const DeleteModal = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <aside className='modal-container'>
+      <div className='modal'>
+        <div className='message'>
+          Your contact has been deleted successfully!
+        </div>
+        <div className='btn-container'>
+          <button
+            onClick={() => {
+              dispatch(CloseDeleteSuccessModal());
+            }}
+            className='modal-btn'
+          >
+            Okay
+          </button>
+        </div>
+      </div>
+    </aside>
+  );
+};
+export default DeleteModal;
---
+++ src/Components/SaveModal.jsx
@@ -0,0 +1,24 @@
+import { useDispatch } from 'react-redux';
+import { CloseSaveModal } from '../Features/ModalSlice';
+const Modal = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <aside className='modal-container'>
+      <div className='modal'>
+        <div className='message'>Your contact has been saved successfully!</div>
+        <div className='btn-container'>
+          <button
+            onClick={() => {
+              dispatch(CloseSaveModal());
+            }}
+            className='modal-btn'
+          >
+            Okay
+          </button>
+        </div>
+      </div>
+    </aside>
+  );
+};
+export default Modal;
