57de3f9dd3a616dd54453ba661e81e6f48c62f13
Eyshcharya <eyshcharya@gmail.com>
2023-08-31 22:29:33 +0530

    Modified the schemas

--- src/Utils/FormValidation.jsx
+++ src/Utils/FormValidation.jsx
@@ -2,15 +2,18 @@
 import * as yup from "yup";
 
 export const LoginSchema = yup.object().shape({
-  email: yup.string().email().required(`Invalid email`),
-  password: yup.string().required().min(6),
+  email: yup.string().email(`Invalid email`).required(`Email is Required`),
+  password: yup.string().required(`Password is Required`),
 });
 
 export const RegisterSchema = yup.object().shape({
-  email: yup.string().email().required(),
-  password: yup.string().required(),
+  email: yup.string().email(`Invalid email`).required(`Email is Required`),
+  password: yup
+    .string()
+    .required(`Password is Required`)
+    .min(6, `Password should contain minimum 6 characters`),
   confirmPassword: yup
     .string()
-    .required()
-    .oneOf([yup.ref("password"), null], "passwords must match"),
+    .required(`Confirm the Password`)
+    .oneOf([yup.ref("password"), null], "Passwords don't match"),
 });
