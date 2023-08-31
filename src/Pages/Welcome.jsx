658a75d0461bbe188ed6392fe0f08abe4d263a82
Eyshcharya <eyshcharya@gmail.com>
2023-08-31 20:30:19 +0530

    Added an useEffect to navigate to Login

--- src/Pages/Welcome.jsx
+++ src/Pages/Welcome.jsx
@@ -1,35 +1,37 @@
-import SharedLayout from '../Components/SharedLayout.jsx';
-import Login from './Login.jsx';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useEffect } from "react";
+import SharedLayout2 from "../Layouts/SharedLayout2.jsx";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Welcome = () => {
   const { isLogin } = useSelector((store) => store.home);
-  const { contactArray } = useSelector((store) => store.contact);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLogin) {
+      navigate(`/login`);
+    }
+  }, [isLogin]);
   return (
     <>
-      {isLogin ? (
-        <Login />
-      ) : (
-        <>
-          <SharedLayout />
+      <SharedLayout2 />
 
-          <div className='welcome-page-body'>
-            <p>
-              <span>Welcome,</span> <br />
-              This is where your contacts will live. Click the button below to
-              add a new contact.
-            </p>
-            <button id='add-contact-btn'>
-              <Link to='/contacts/new'>
-                {contactArray.length < 1
-                  ? 'Add your first contact'
-                  : 'Add new contacts'}
-              </Link>
-            </button>
-          </div>
-        </>
-      )}
+      <div className='welcome-page-body'>
+        <p>
+          <span>Welcome,</span> <br />
+          This is where your contacts will live. Click the button below to add a
+          new contact.
+        </p>
+        <button id='add-contact-btn'>
+          <Link to='/contacts/new'>
+            {/* {contactArray.length < 1
+                  ? "Add your first contact"
+                  : "Add new contacts"} */}
+            sth
+          </Link>
+        </button>
+      </div>
     </>
   );
 };
