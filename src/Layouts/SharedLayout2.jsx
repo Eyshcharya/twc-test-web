cfef8d2d4e9665a43706d1ffc270a423a681c33e
Eyshcharya <eyshcharya@gmail.com>
2023-08-31 22:29:58 +0530

    Implemented the logout mutation

--- src/Layouts/SharedLayout2.jsx
+++ src/Layouts/SharedLayout2.jsx
@@ -2,13 +2,23 @@ import { useDispatch } from "react-redux";
 import { Icon, LogoutIcon } from "../assets/logo.jsx";
 import { Link, useNavigate } from "react-router-dom";
 import { NavigateToLogin } from "../Slices/homeSlice.jsx";
+import { useLogoutMutation } from "../Slices/API/actionsApiSlice.js";
+import { toast } from "react-toastify";
+
 const SharedLayout2 = () => {
+  const [logout] = useLogoutMutation();
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    dispatch(NavigateToLogin());
-    navigate(`/login`);
+  const handleLogout = async () => {
+    try {
+      await logout().unwrap;
+      toast.success("Logout Success!");
+      dispatch(NavigateToLogin());
+      navigate(`/login`);
+    } catch (error) {
+      toast.error("Logout Failed");
+    }
   };
   return (
     <div className='shared-layout'>
