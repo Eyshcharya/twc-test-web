ad1ac4d210ce27a48ea39d34b258e379c23135d3
Eyshcharya <eyshcharya@gmail.com>
2023-08-31 16:31:28 +0530

    Implemented the store with redux persist

---
+++ src/Store/Store.jsx
@@ -0,0 +1,38 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage/session";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+import { apiSlice } from "../Slices/API/apiSlice";
+import homeReducer from "../Slices/homeSlice";
+import modalReducer from "../Slices/ModalSlice";
+// import contactReducer from "../Slices/contactSlice";
+
+// redux-persist config
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+//combine reducers
+const reducer = combineReducers({
+  home: homeReducer,
+  modal: modalReducer,
+  [apiSlice.reducerPath]: apiSlice.reducer,
+});
+
+// persist reducer
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export const Store = configureStore({
+  reducer: persistedReducer,
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: ["persist/PERSIST"],
+      },
+    }).concat(apiSlice.middleware),
+  devTools: true,
+});
