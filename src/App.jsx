59d52d443fbca692ab7042b9b753cf7c13fa127c
Eyshcharya <eyshcharya@gmail.com>
2023-08-31 20:25:51 +0530

    Updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Welcome from './Pages/Welcome';
-import Login from './Pages/Login';
-import NewContact from './Pages/NewContact';
-import Contacts from './Pages/Contacts';
-import Error from './Components/Error';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Welcome from "./Pages/Welcome";
+import Login from "./Pages/Login";
+import { Register } from "./Pages/Register";
+import NewContact from "./Pages/NewContact";
+import Contacts from "./Pages/Contacts";
+import Error from "./Components/Error";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer />
       <Routes>
-        <Route path='login' element={<Login />} />
-
         <Route path='/' element={<Welcome />} />
+        <Route path='login' element={<Login />} />
+        <Route path='register' element={<Register />} />
 
         <Route path='contacts'>
           <Route index element={<Contacts />} />
