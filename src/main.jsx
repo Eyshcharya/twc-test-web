0b1a7133c2423c5547b1a4040e069d371774f188
Eyshcharya <eyshcharya@gmail.com>
2023-08-31 20:27:38 +0530

    Imported toastify styles and implemented redux persist gate

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,18 @@ import App from "./App.jsx";
 import "./index.css";
 import { Store } from "./Store/Store.jsx";
 import { Provider } from "react-redux";
+import "react-toastify/dist/ReactToastify.css";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+let persistor = persistStore(Store);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={Store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
