cef285254b861fbc1c4daa630400d8c2f6a6cf19
Eyshcharya <eyshcharya@gmail.com>
2023-09-01 16:34:44 +0530

    Added userID to the  state

--- src/Slices/homeSlice.jsx
+++ src/Slices/homeSlice.jsx
@@ -3,11 +3,15 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   isLogin: false,
   email: "",
+  userID: "",
 };
 const homeSlice = createSlice({
   name: "home",
   initialState,
   reducers: {
+    setUserID: (state, { payload }) => {
+      state.userID = payload;
+    },
     navigateWelcomePage: (state, { payload }) => {
       state.isLogin = true;
       state.email = payload;
@@ -15,8 +19,10 @@ const homeSlice = createSlice({
     NavigateToLogin: (state) => {
       state.isLogin = false;
       state.email = null;
+      state.userID = null;
     },
   },
 });
-export const { navigateWelcomePage, NavigateToLogin } = homeSlice.actions;
+export const { setUserID, navigateWelcomePage, NavigateToLogin } =
+  homeSlice.actions;
 export default homeSlice.reducer;
