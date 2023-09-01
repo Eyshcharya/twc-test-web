3b88a9c56cae99a5bfd95882a9d303f11a814688
Eyshcharya <eyshcharya@gmail.com>
2023-09-01 16:35:23 +0530

    Implemented the getUser endpoint.

--- src/Pages/Welcome.jsx
+++ src/Pages/Welcome.jsx
@@ -1,22 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import SharedLayout2 from "../Layouts/SharedLayout2.jsx";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { useGetUserMutation } from "../Slices/API/contactsApiSlice.js";
+import { toast } from "react-toastify";
+import { setUserID } from "../Slices/homeSlice.jsx";
 
 const Welcome = () => {
-  const { isLogin } = useSelector((store) => store.home);
+  const { isLogin, email } = useSelector((store) => store.home);
+  const [User] = useGetUserMutation();
+  const [user, setUser] = useState(null);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (!isLogin) {
       navigate(`/login`);
+    } else {
+      getUser();
     }
   }, [isLogin]);
+
+  const getUser = async () => {
+    User(email)
+      .unwrap()
+      .then((data) => {
+        setUser(data);
+        dispatch(setUserID(data._id));
+      })
+      .catch((error) => {
+        toast.error(error.data?.message);
+      });
+  };
+
   return (
     <>
       <SharedLayout2 />
-
       <div className='welcome-page-body'>
         <p>
           <span>Welcome,</span> <br />
@@ -25,10 +45,9 @@ const Welcome = () => {
         </p>
         <button id='add-contact-btn'>
           <Link to='/contacts/new'>
-            {/* {contactArray.length < 1
-                  ? "Add your first contact"
-                  : "Add new contacts"} */}
-            sth
+            {user?.contacts.length <= 0
+              ? "Add your first contact"
+              : "Add new contacts"}
           </Link>
         </button>
       </div>
